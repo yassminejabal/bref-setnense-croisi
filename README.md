74112a597b35c5b770be6413284e653ad4d09950
yassmine <jabalyassmine2007@gmail.com>
2025-11-18 19:11:58 +0100

    j'ai faire deux etape la premier etape j'ai veis faire la formulaire et sa validation dans la premier jour et j'ai faishtml css de formulair et j'ai sidbar

---
+++ README.md
@@ -0,0 +1,29 @@
+# WorkSphere – Gestion du Personnel #
+## Description
+WorkSphere est une application web pour gérer le personnel sur un plan d’étage.  
+Elle permet d’ajouter, déplacer et supprimer des employés, en respectant les rôles et les zones autorisées.
+## Fonctionnalités
+- Ajouter et supprimer des employés
+- Affectation automatique selon le rôle et la zone
+- Visualisation des employés non assignés
+- Profil détaillé des employés (photo, email, téléphone, expériences)
+- Interface responsive pour ordinateur, tablette et mobile
+- Limitation du nombre d’employés par zone
+- Zones vides visibles pour mieux organiser l’espace
+## Technologies
+- HTML, CSS, JavaScript
+- Flexbox pour le design
+- Animations CSS pour fluidité
+- Déploiement possible sur GitHub Pages
+## structure
+worksphere/             
+├── index.html          
+├── README.md           
+│
+├── css/
+│   └── style.css       
+│
+├── img/
+    ├── employees/      
+    └── icons/
+    
---
+++ javascript.js
@@ -0,0 +1,288 @@
+let unassignedEmployees = [];
+const form = document.getElementById("form");
+const inputbutton = document.getElementById("input-button");
+const btnAddWorkerBtn = document.getElementById("btnaddworkerBtn");
+const sidebar = document.getElementById("sidebar");
+form.style.display = "none";
+const nomprenom = document.getElementById("input-Nom-Prenom");
+const email = document.getElementById("input-Email");
+const role = document.getElementById("input-Role");
+const telephone = document.getElementById("input-telephone");
+const experiences = document.getElementById("input-Expériences");
+const localisation = document.getElementById("loc");
+
+btnAddWorkerBtn.addEventListener("click", () => {
+    form.style.display = "block";
+});
+
+function affchesidbar() {
+    sidebar.classList.toggle("active");
+    document.body.classList.toggle('sidebar-open');
+}
+
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const nomregix = /^[^\d]+$/;
+    const emailregix = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const roleregix = /^[^\d]+$/;
+    const telregix = /^\+?\d{8,15}$/;
+
+    const nomPrenomvalue = nomprenom.value.trim();
+    const emailvalue = email.value.trim();
+    const rolevalue = role.value.trim();
+    const telephonevalue = telephone.value.trim();
+    const experiencesvalue = experiences.value.trim();
+    const localisationvalue = localisation.value.trim();
+
+    if (!nomregix.test(nomPrenomvalue)) {
+        alert("Nom et prénom incorrect(s)");
+        return;
+    }
+    if (!emailregix.test(emailvalue)) {
+        alert("Email incorrect");
+        return;
+    }
+    if (!roleregix.test(rolevalue)) {
+        alert("Role incorrect");
+        return;
+    }
+    if (telephonevalue && !telregix.test(telephonevalue)) {
+        alert("Téléphone incorrect");
+        return;
+    }
+    if (!experiencesvalue) {
+        alert("Veuillez indiquer les expériences");
+        return;
+    }
+
+    unassignedEmployees.push({
+         nomPrenomvalue,
+         emailvalue,
+        rolevalue,
+        telephonevalue,
+        experiencesvalue,
+        localisationvalue
+    });
+
+    console.log(unassignedEmployees);
+    form.reset();
+    form.style.display = "none";
+    
+    renderSidebar();
+    
+});
+
+function renderSidebar(){
+   const sidebar = document.getElementById("sidebar");
+    unassignedEmployees.forEach((card,index)=>{
+            const carde = document.createElement("div");
+            carde.classList.add("card");
+                    carde.innerHTML=`
+
+            <img src="https://avatar.iran.liara.run/public/99" alt="photo">
+            <h2>${card.nomPrenomvalue}</h2>
+            <p>${card.emailvalue}</p>
+            <p>${card.rolevalue}</p>
+            <p>${card.telephonevalue}</p>
+            <h3>${card.experiencesvalue}</h3>
+            <ul>
+                <li>${card.localisationvalue}</li>
+            </ul>
+            <button onclick="modal(${(index)})"">click</button>
+    `
+    sidebar.appendChild(carde);
+    }
+)
+}
+
+
+
+// function modal(index){
+//     // const btnd = document.getElementById("btnb");
+    
+//     const nomPrenomvalue = nomprenom.value.trim();
+//     const emailvalue = email.value.trim();
+//     const rolevalue = role.value.trim();
+//     const telephonevalue = telephone.value.trim();
+//     const experiencesvalue = experiences.value.trim();
+//     const localisationvalue = localisation.value.trim();
+//     unassignedEmployees.forEach((card)=>
+//                     card[index].innerHTML=`
+//             <img src="https://avatar.iran.liara.run/public/99" alt="Photo">
+
+
+//             <h2>${nomPrenomvalue}</h2>
+//             <p>${emailvalue}</p>
+//             <p>${rolevalue}</p>
+//             <p>${telephonevalue}</p>
+//             <h3>${experiencesvalue}</h3>
+//             <ul>
+//                 <li>${localisationvalue}</li>
+
+//             </ul>`
+
+// )}
+
+
+
+
+
+
+
+
+
+// function renderSidebar() {
+//     //     const nomPrenomvalue = nomprenom.value.trim();
+//     // const emailvalue = email.value.trim();
+//     // const rolevalue = role.value.trim();
+//     // const telephonevalue = telephone.value.trim();
+//     // const experiencesvalue = experiences.value.trim();
+//     // const localisationvalue = localisation.value.trim();
+
+
+//     unassignedEmployees.forEach((emp, index) => {
+//         const card = document.createElement("div");
+//         card.className = "card";
+//         card.innerHTML = `
+//             <div class="btns">
+//                 <button class="removeBtn">X</button>
+//             </div>
+//             <img src="https://avatar.iran.liara.run/public" alt="Photo">
+//             <h2>${emp.nomPrenomvalue}</h2>
+//             <p><strong>Rôle:</strong> ${emp.rolevalue}</p>
+//             <p><strong>Email:</strong> ${emp.emailvalue}</p>
+//             <p><strong>Téléphone:</strong> ${emp.telephonevalue}</p>
+//             <h3>Expériences :</h3>
+//             <p>${emp.experiencesvalue}</p>
+//             <p><strong>Localisation:</strong> ${emp.localisationvalue}</p>
+//         `
+
+
+// })
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //     // remove button handler
+    //     card.querySelector('.removeBtn').addEventListener('click', (ev) => {
+    //         const idx = Number(ev.target.getAttribute('data-index'));
+    //         unassignedEmployees.splice(idx, 1);
+    //         renderSidebar();
+    //     });
+
+    //     sidebar.appendChild(card);
+    // });
+
+
+
+
+
+
+
+
+    // if (unassignedEmployees.length === 0) {
+    //     sidebar.innerHTML += `<p>Aucun employé non affecté.</p>`;
+    //     return;
+    // }
+
+
+
+
+
+
+
+
+
+// function modal(index){
+//     
+//     console.log(cardi);
+//     unassignedEmployees.forEach((index)=>{
+ 
+    
+//     `
+//     }
+//     document.body.append(cardi);
+
+//     }
+          
+
+
+//    cardi[index].innerHTML=`
+            
+
+//             <img src="https://avatar.iran.liara.run/public/99" alt="Photo">
+
+//             <h2>${cardi[index].nomprenom}</h2>
+//             <p>${cardi[index].emailvalue}</p>
+//             <p>${cardi[index].rolevalue}</p>
+//             <p>${cardi[index].telephonevalue}</p>
+//             <h3>${cardi[index].experiencesvalue}</h3>
+//             <ul>
+//                 <li>${cardi[index].localisationvalue}</li>
+
+//             </ul>
+
+
+
+
+
+
+// let unassgnedEmployees = [
+//     {
+//         name: "efzef",
+
+//     }
+// ]
+
+
+// let assgnedEmployees = {
+//     room1: [
+//         {
+//             name: "efzef",
+
+//         },
+//         {
+//             name: "efzef",
+
+//         }
+//     ],
+
+// }
